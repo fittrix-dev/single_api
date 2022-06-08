7ac34ae17b8a739271f943c2775cd765f7c90e3e
minaraeFittrix <89445673+minaraeFittrix@users.noreply.github.com>
2022-06-08 15:43:54 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Fittrix'
+copyright = '2022, fittrix'
+author = 'minarae'
 
 release = '0.1'
 version = '0.1.0'
