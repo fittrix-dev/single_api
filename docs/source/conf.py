158ca03f584cef81c1b5a540b38fa8f368ab6dba
minaraeFittrix <89445673+minaraeFittrix@users.noreply.github.com>
2022-06-09 14:44:10 +0900

    롤백

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,23 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 
 
-import os
-import sys
-
-import sphinx_rtd_theme
-
-sys.path.insert(0, os.path.abspath(".."))
-sys.path.append(os.path.dirname(__file__))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.dev")
-
-# Load Django after sys.path and configuration setup
-# isort: split
-import django
-
-django.setup()
-
-sys.path.append(os.path.abspath("_ext"))
-
 # -- Project information
 
 project = 'Fittrix'
