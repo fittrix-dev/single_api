06118280eb2281472dd59973b4f8506cd58b77de
minaraeFittrix <89445673+minaraeFittrix@users.noreply.github.com>
2022-06-09 14:47:06 +0900

    api 테스트 #11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.httpdomain',
+    'sphinxcontrib.httpdomain2',
 ]
 
 intersphinx_mapping = {
